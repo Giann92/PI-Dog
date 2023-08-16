29b448c9a211a90aeb5396e43758aeec141585ce
Giann92 <56614039+Giann92@users.noreply.github.com>
2023-08-15 21:46:31 -0300

    new correciones

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://pi-dog-production-7a02.up.railway.app/";
 const backgroundImages = {
   '/': 'https://helios-i.mashable.com/imagery/articles/07CIEf4aHNEhXeK7MSpNYt5/hero-image.fill.size_1248x702.v1648747007.jpg',
   '/dogs': 'https://t3.ftcdn.net/jpg/05/52/99/70/360_F_552997034_k5EkYzAnUnZVpnsbuQHz1ue4QJCWdDsV.jpg'
