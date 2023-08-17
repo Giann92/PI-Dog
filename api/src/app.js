b0d7557f45fb344f5b923704332a6d01f79b1719
Giann92 <56614039+Giann92@users.noreply.github.com>
2023-08-16 22:43:18 -0300

    ybue

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors'); // Agrega esta línea
 const routes = require('./routes/index.js');
 
 require('./db.js');
@@ -12,12 +13,13 @@ server.name = 'API';
 const url = 'https://pi-dog-self.vercel.app'
 const local = 'http://localhost:3000/'
 
+server.use(cors()); // Agrega esta línea para configurar CORS
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', url); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', url);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -27,7 +29,7 @@ server.use((req, res, next) => {
 server.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
@@ -35,3 +37,4 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 });
 
 module.exports = server;
+
